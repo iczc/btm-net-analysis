27e34d330db2c4169d67e32b3c9fc99656145c2b
iczc <iczcalan@gmail.com>
2019-03-28 21:22:33 +0800

    优化模式优先级

--- README.md
+++ README.md
@@ -22,10 +22,11 @@ Options:
 | Option (short) | Options (long) | Explanation |
 | :------------- | :------------- |:-------------|
 | -h | --help | 查看使用帮助 |
-| -b \<block_height\> | --block \<block_height\> | 查看此区块最早收到的节点的时间，最晚收到的节点的时间，交易从出现到完全广播花费的时间 |
-| -b all | --block all | 查看所有区块完全广播的最短时间，最长时间，平均值，中位数 |
 | -t \<tx_hash\> | --transaction \<tx_hash\> | 查看此交易最早收到的节点的时间，最晚收到的节点的时间，交易从出现到完全广播话费的时间 |
 | -t all | --transaction all | 查看所有交易完全广播的最短时间，最长时间，平均值，中位数 |
+| -b \<block_height\> | --block \<block_height\> | 查看此区块最早收到的节点的时间，最晚收到的节点的时间，交易从出现到完全广播花费的时间 |
+| -b all | --block all | 查看所有区块完全广播的最短时间，最长时间，平均值，中位数 |
+
 
 
 ## 日志参数说明
--- args.py
+++ args.py
@@ -13,7 +13,7 @@ class ProcessArgs:
     def __init__(self, argv):
         """初始化命令行参数、类属性并解析命令行参数"""
         self.__argv = argv # 设置命令行设置
-        self.current_mode = 0 # 模式1:分析单个区块信息 模式2:分析所有区块 模式3:分析单笔交易 模式4:分析所有交易
+        self.current_mode = 0 # 模式1:分析单笔交易 模式2:分析所有交易 模式3:分析单个区块 模式4:分析所有区块
         self.height = None # 区块高度
         self.tx_hash = None # 交易Hash
         self.__parse_args()  # 解析参数
@@ -25,7 +25,7 @@ class ProcessArgs:
             self.__exit(0)
         # 如果使用了的命令行参数超过一个打印帮助后退出
         if len(self.__argv) > 2:
-            print('Only one of -b/--block and -t/--transaction can be used!')
+            print('Only one of -t/--transaction and -b/--block can be used!')
             self.__exit(-1)
         # 解析命令行参数
         try:
@@ -36,26 +36,26 @@ class ProcessArgs:
         for opt, arg in opts:
             if opt in ('-h', '--help'):
                 self.__exit(0)
-            elif opt in ('-b', '--block'):
-                # 判读参数是否为all
-                if arg == 'all':
-                    self.current_mode = 2
-                # 判断传入的区块高度的字符串是否是数字内容
-                elif arg.isdigit():
-                    self.current_mode = 1
-                    self.height = int(arg)
-                else:
-                    print('Please confirm the parameter is block height!')
-                    self.__exit(-1)
             elif opt in ('-t', '--transaction'):
                 if arg == 'all':
-                    self.current_mode = 4
+                    self.current_mode = 2
                 # 判读传入的交易id是否为长度64的tx_hash
                 elif len(arg) == 64:
-                    self.current_mode = 3
+                    self.current_mode = 1
                     self.tx_hash = arg
                 else:
                     print('Please confirm the parameter is transaction hash!')
+                    self.__exit(-1)
+            elif opt in ('-b', '--block'):
+                # 判读参数是否为all
+                if arg == 'all':
+                    self.current_mode = 4
+                # 判断传入的区块高度的字符串是否是数字内容
+                elif arg.isdigit():
+                    self.current_mode = 3
+                    self.height = int(arg)
+                else:
+                    print('Please confirm the parameter is block height!')
                     self.__exit(-1)
             else:
                 logging.error('unhandled option')
@@ -67,15 +67,15 @@ class ProcessArgs:
         """打印使用帮助并使用传入的错误码退出程序"""
         print("\nUsage: main.py "
             "[-h/--help] "
-            "[-b/--block <block_height>/all] "
-            "[-t/--transaction <tx_hash>/all]")
+            "[-t/--transaction <tx_hash>/all]"
+            "[-b/--block <block_height>/all] ")
         sys.exit(exit_code)
 
     def __print_args(self):
         """打印参数信息"""
-        mode_info = ['', 'Analyze a block', 'Analyze all blocks', 'Analyze a transaction', 'Analyze all transactions']
+        mode_info = ['', 'Analyze a transaction', 'Analyze all transactions', 'Analyze a block', 'Analyze all blocks']
         print('current mode is %s' %(mode_info[self.current_mode]))
         if self.current_mode == 1:
-            print('block height is %s' %str(self.height))
-        elif self.current_mode == 3:
             print('transaction id is %s' %self.tx_hash)
+        elif self.current_mode == 3:
+            print('block height is %s' %self.height)
