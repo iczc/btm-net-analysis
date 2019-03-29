ba4ba4c92d0c3d9f3965151ee636b3308d688331
iczc <iczcalan@gmail.com>
2019-03-29 20:00:29 +0800

    生成交易与区块信息字典

--- log.py
+++ log.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import datetime
 import re
-
-from util import logtime_to_millisecondtimestamp
 
 
 class LogProcessing:
     def __init__(self, log_path):
-        self.__log_path = log_path
-        self.transaction_log_list = []
-        self.block_log_list = []
-        self.__execution_clean_log()
-
-    def __clean_divided_log(self, divided_log):
-        # 在list中删除单条日志中msg和type
-        del(divided_log[4])
-        del(divided_log[1])
-        # 将日志中的时间格式转换为毫秒级的时间戳
-        # divided_log[0] = logtime_to_millisecondtimestamp(divided_log[0])
-        # 交换列表元素位置
-        # divided_log[0], divided_log[1] = divided_log[1], divided_log[0]
-        return divided_log
+        self.__log_path = log_path # 日志文件路径
+        self.transaction_log_list = [] # 清洗后的交易日志列表 [['时间', '交易id', '节点ip:端口']]
+        self.block_log_list = [] # 清洗后的交易日志列表 [['时间', '区块高度', '节点ip:端口']]
+        self.transaction_dict = None # 字典格式的交易信息 {'交易id': [['时间','节点ip:端口']]}
+        self.block_dict = None # 字典格式的区块信息 {'区块高度': [['时间','节点ip:端口']]}
+        self.__execution_clean_log() # 执行日志清洗
     
     def __execution_clean_log(self):
         with open(self.__log_path, 'r') as f:
             # 将日志文件以行为单位拆分并放入list
             log_list = f.readlines()
         for i in range(0, len(log_list)):
-            # 使用正则表达式匹配双引号中的内容并存为list
+            # 使用正则表达式匹配双引号中的内容并存为列表
             # 格式为:['Mar 17 00:00:00.486', 'receive message from peer', '{height: 197197}', '115.54.192.9:52618', '*netsync.GetBlockMessage']
             divided_log = re.findall(r'\"([^\"]*)\"', log_list[i])
             # 日志类型
@@ -37,36 +26,36 @@ class LogProcessing:
             if log_type == '*netsync.TransactionMessage':
                 # 截取交易message中的交易id
                 divided_log[2] = divided_log[2][-65:-1]
-                divided_log = self.__clean_divided_log(divided_log)
+                # 删除列表中的type属性
+                del(divided_log[-1])
+                # 删除列表中的msg属性
+                del(divided_log[1])
                 self.transaction_log_list.append(divided_log)
             elif log_type == '*netsync.MineBlockMessage':
                 # 截取区块message中的区块高度
                 divided_log[2] = int(divided_log[2][15:-79])
-                divided_log = self.__clean_divided_log(divided_log)
+                del(divided_log[-1])
+                del(divided_log[1])
                 self.block_log_list.append(divided_log)
 
     def generate_transaction_dictionary(self):
         # 取transaction_log_list中的第二列即交易id 包含重复元素
         all_tx_hash = list(zip(*self.transaction_log_list))[1]
         # 从all_tx_hash元组中创建字典 key为交易id 同时去处重复元素
-        transaction_dict = dict().fromkeys(all_tx_hash, [])
-        print(transaction_dict)
+        self.transaction_dict = dict().fromkeys(all_tx_hash, [])
+        for i in range(0, len(self.transaction_log_list)):
+            # 设置区块字典的key为交易id
+            transaction_dict_key = self.transaction_log_list[i][1]
+            # 设置交易字典的key为['时间', '节点ip:端口']
+            transaction_dict_value = [self.transaction_log_list[i][0], self.transaction_log_list[i][2]]
+            # 构建字典
+            self.transaction_dict[transaction_dict_key].append(transaction_dict_value)
 
     def generate_block_dictionary(self):
-        # 取block_log_list中的第二列即区块高度 包含重复元素
         all_block_height = list(zip(*self.block_log_list))[1]
-        # 从all_block_height元组中创建字典 key为区块高度 同时去处重复元素
-        block_dict = dict().fromkeys(all_block_height, [])
-        print(block_dict)
-
-if __name__ == "__main__":
-    starttime = datetime.datetime.now()
-    log_file = 'log_data/bytomd_20190304183016.log'
-    log = LogProcessing(log_file)
-    log.generate_transaction_dictionary()
-    log.generate_block_dictionary()
-    # print(log.block_log_list)
-    # print(log.transaction_log_list)
-    endtime = datetime.datetime.now()
-    print((endtime - starttime).seconds)
-    print((endtime - starttime).microseconds)
+        self.block_dict = dict().fromkeys(all_block_height, [])
+        for i in range(0, len(self.block_log_list)):
+            # 设置区块字典的key为区块高度
+            block_dict_key = self.block_log_list[i][1]
+            block_dict_value = [self.block_log_list[i][0], self.block_log_list[i][2]]
+            self.block_dict[block_dict_key].append(block_dict_value)
