eb71f4385088fb46d9efc2f54522f7689a6a68a4
iczc <iczcalan@gmail.com>
2019-04-02 08:55:23 +0800

    子线程处理文件主线程分析数据

---
+++ data.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from threading import Thread
+
+from log import LogProcessing
+
+
+class DataThread(Thread):
+    def __init__(self,log_file, log_dict_list, type):
+        super().__init__()
+        self.__log_file = log_file
+        # 列表当作参数传递给线程会当作全局变量
+        self.log_dict_list = log_dict_list
+        self.__type = type
+        self.__log_dict = dict()
+        self.__log = LogProcessing(self.__log_file)
+
+    def run(self):
+        if self.__type == 'transaction':
+            self.__create_transactio_dict()
+        elif self.__type == 'block':
+            self.__create__block_dict()
+        # append为线程安全方法无需互斥锁
+        self.log_dict_list.append(self.__log_dict)
+
+    def __create_transactio_dict(self):
+        self.__log.generate_transaction_dictionary()
+        self.__log_dict = self.__log.transaction_dict
+    
+    def __create__block_dict(self):
+        self.__log.generate_block_dictionary()
+        self.__log_dict = self.__log.block_dict
--- main.py
+++ main.py
@@ -8,44 +8,12 @@ from queue import Queue
 
 from args import ArgsProcessing
 from log import LogProcessing
-from analysis import AnalysisThread
+from data import DataThread
 
 from util import calc_time_interval
 
 assert sys.version_info >= (3, 6, 0), 'btm-net-analysis requires Python 3.6+'  # 检查Python版本
 
-
-def analyze_single(log_file_list, parameter, type):
-    overall_earliest_msg = []
-    overall_latest_msg = []
-    q = Queue(len(log_file_list) * 2)
-    for file in log_file_list:
-        tx = AnalysisThread(q, file, parameter, type=type)
-        tx.start()
-        tx.join()
-    if q.empty():
-        print('The %s %s was not found in log file!' %(type, parameter))
-        sys.exit(0)
-    while True:
-        # 队列长度为偶数时队列头部为最早的时间
-        if (q.qsize() % 2 == 0):
-            earliest_msg = q.get()
-            print('Earliest_msg: %s' %earliest_msg)
-            # 如果全局的最早时间为空或大于队列中的时间则更新
-            if not overall_earliest_msg or LogProcessing.logtime_to_millisecondtimestamp(earliest_msg[0]) \
-                < LogProcessing.logtime_to_millisecondtimestamp(overall_earliest_msg[0]):
-                overall_earliest_msg = earliest_msg
-        else:
-            latest_msg = q.get()
-            print('Latest_msg:   %s' %latest_msg)
-            if not overall_latest_msg or LogProcessing.logtime_to_millisecondtimestamp(latest_msg[0]) \
-                > LogProcessing.logtime_to_millisecondtimestamp(overall_latest_msg[0]):
-                overall_latest_msg = latest_msg
-        if q.empty():
-            break
-    print('最早: %s' %overall_earliest_msg)
-    print('最晚: %s' %overall_latest_msg)
-    print('间隔: %s' %calc_time_interval(overall_latest_msg[0], overall_earliest_msg[0]))
 
 def main():
     args_info = ArgsProcessing(sys.argv[1:])  # 处理命令行参数
@@ -55,14 +23,63 @@ def main():
     # 模式1:分析单笔交易 模式2:分析所有交易 模式3:分析单个区块 模式4:分析所有区块
     if work_mode == 1:
         tx_hash = args_info.tx_hash # 交易hash
-        analyze_single(log_file_list, tx_hash, type='transaction')
+        # analyze_single(log_file_list, tx_hash, type='transaction')
+        all_tx_dict_list = []
+        # 所有日志文件中最早的消息
+        overall_earliest_msg = []
+        # 所有日志文件中最晚的消息
+        overall_latest_msg = []
+        for log_file in log_file_list:
+            data = DataThread(log_file, all_tx_dict_list, type='transaction')
+            data.start()
+            data.join()
+        for tx_dict in all_tx_dict_list:
+            if tx_hash in tx_dict:
+                data_list = tx_dict[tx_hash]
+                earliest_msg = data_list[0]
+                latest_msg = data_list[-1]
+                print('Earliest_msg: %s' %earliest_msg)
+                print('Latest_msg:   %s' %latest_msg)
+                if not overall_earliest_msg or LogProcessing.logtime_to_millisecondtimestamp(earliest_msg[0]) \
+                    < LogProcessing.logtime_to_millisecondtimestamp(overall_earliest_msg[0]):
+                    overall_earliest_msg = earliest_msg
+                if not overall_latest_msg or LogProcessing.logtime_to_millisecondtimestamp(latest_msg[0]) \
+                    > LogProcessing.logtime_to_millisecondtimestamp(overall_latest_msg[0]):
+                    overall_latest_msg = latest_msg
+        print('最早: %s' %overall_earliest_msg)
+        print('最晚: %s' %overall_latest_msg)
     elif work_mode == 2:
         pass
     elif work_mode == 3:
         height = args_info.height # 区块高度
-        analyze_single(log_file_list, height, type='block')
+        all_block_dict_list = []
+        # 所有日志文件中最早的消息
+        overall_earliest_msg = []
+        # 所有日志文件中最晚的消息
+        overall_latest_msg = []
+        for log_file in log_file_list:
+            data = DataThread(log_file, all_block_dict_list, type='block')
+            data.start()
+            data.join()
+        for block_dict in all_block_dict_list:
+            if height in block_dict:
+                data_list = block_dict[height]
+                earliest_msg = data_list[0]
+                latest_msg = data_list[-1]
+                print('Earliest_msg: %s' %earliest_msg)
+                print('Latest_msg:   %s' %latest_msg)
+                if not overall_earliest_msg or LogProcessing.logtime_to_millisecondtimestamp(earliest_msg[0]) \
+                    < LogProcessing.logtime_to_millisecondtimestamp(overall_earliest_msg[0]):
+                    overall_earliest_msg = earliest_msg
+                if not overall_latest_msg or LogProcessing.logtime_to_millisecondtimestamp(latest_msg[0]) \
+                    > LogProcessing.logtime_to_millisecondtimestamp(overall_latest_msg[0]):
+                    overall_latest_msg = latest_msg
+        print('最早: %s' %overall_earliest_msg)
+        print('最晚: %s' %overall_latest_msg)
+        print('间隔: %s' %calc_time_interval(overall_latest_msg[0], overall_earliest_msg[0]))
     elif work_mode == 4:
         pass
+
     print('分析用时: ', time.time()-start_time)
 
 if __name__ == '__main__':
