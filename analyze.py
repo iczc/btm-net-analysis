749eb09a46aa5e34a96b23d762a15fc8f9ecff22
iczc <iczcalan@gmail.com>
2019-04-03 11:23:46 +0800

    初步实现使用多进程分析所有数据

--- analyze.py
+++ analyze.py
@@ -4,13 +4,18 @@
 from data import DataThread
 from log import LogProcessing
 
+from util import calc_millisecond_interval
+
 
 def get_all_log_dict(log_file_list, type):
+    threads = []
     all_log_dict = []
     for log_file in log_file_list:
         data = DataThread(log_file, all_log_dict, type=type)
         data.start()
-        data.join()
+        threads.append(data)
+    for thread in threads:
+        thread.join()
     return all_log_dict
 
 def retrieve_earliest_latest_msg(all_log_dict, dict_key):
@@ -33,3 +38,12 @@ def retrieve_earliest_latest_msg(all_log_dict, dict_key):
                 > LogProcessing.logtime_to_millisecondtimestamp(overall_latest_msg[0]):
                 overall_latest_msg = latest_msg
     return overall_earliest_msg, overall_latest_msg
+
+def calc_broadcasting_time(work_queue, broadcasting_time_queue, all_log_dict):
+    while True:
+        dict_key = work_queue.get()
+        overall_earliest_msg, overall_latest_msg = retrieve_earliest_latest_msg(all_log_dict, dict_key)
+        millisecond_interval = calc_millisecond_interval(overall_latest_msg[0], overall_earliest_msg[0])
+        broadcasting_time_queue.put(millisecond_interval)
+        if work_queue.empty():
+            break
--- main.py
+++ main.py
@@ -4,10 +4,13 @@
 import sys
 import time
 
+from multiprocessing import Process, Queue, cpu_count
+
 from args import ArgsProcessing
 
 from analyze import get_all_log_dict
 from analyze import retrieve_earliest_latest_msg
+from analyze import calc_broadcasting_time
 from util import calc_millisecond_interval
 from util import millisecond2time_format
 from util import get_average_median
@@ -34,21 +37,32 @@ def main():
             print('The transaction %s was not found in log file!' %tx_hash)
     elif work_mode == 2:
         all_tx_dict_list = get_all_log_dict(log_file_list, type='transaction')
-        broadcasting_time = []
         all_tx_hash = []
         for tx_dict in all_tx_dict_list:
             all_tx_hash.extend(list(tx_dict.keys()))
         # 去除重复元素
         all_tx_hash = list(set(all_tx_hash))
+        tx_hash_queue = Queue()
+        broadcasting_time_queue = Queue()
         for tx_hash in all_tx_hash:
-            overall_earliest_msg, overall_latest_msg = retrieve_earliest_latest_msg(all_tx_dict_list, tx_hash)
-            millisecond_interval = calc_millisecond_interval(overall_latest_msg[0], overall_earliest_msg[0])
-            # print('tx %s : %s' %(tx_hash, millisecond_interval))
-            broadcasting_time.append(millisecond_interval)
-        broadcasting_time.sort()
-        average, median = get_average_median(broadcasting_time)
-        print('最短时间: %s' %millisecond2time_format(broadcasting_time[0]))
-        print('最长时间: %s' %millisecond2time_format(broadcasting_time[-1]))
+            tx_hash_queue.put(tx_hash)
+        processes = []
+        processor_num = cpu_count()
+        for i in range(processor_num):
+            p = Process(target=calc_broadcasting_time, args=(tx_hash_queue, broadcasting_time_queue, all_tx_dict_list))
+            p.start()
+            processes.append(p)
+        for process in processes:
+            process.join()
+        broadcasting_time_list = []
+        while True:
+            broadcasting_time_list.append(broadcasting_time_queue.get())
+            if broadcasting_time_queue.empty():
+                break
+        broadcasting_time_list.sort()
+        average, median = get_average_median(broadcasting_time_list)
+        print('最短时间: %s' %millisecond2time_format(broadcasting_time_list[0]))
+        print('最长时间: %s' %millisecond2time_format(broadcasting_time_list[-1]))
         print('平均值:   %s' %millisecond2time_format(average))
         print('中位数:   %s' %millisecond2time_format(median))
     elif work_mode == 3:
@@ -64,23 +78,34 @@ def main():
             print('The block height %s was not found in log file!' %height)
     elif work_mode == 4:
         all_block_dict_list = get_all_log_dict(log_file_list, type='block')
-        broadcasting_time = []
         all_block_height = []
         for block_dict in all_block_dict_list:
             all_block_height.extend(list(block_dict.keys()))
         all_block_height = list(set(all_block_height))
+        block_queue = Queue()
+        broadcasting_time_queue = Queue()
         for height in all_block_height:
-            overall_earliest_msg, overall_latest_msg = retrieve_earliest_latest_msg(all_block_dict_list, height)
-            millisecond_interval = calc_millisecond_interval(overall_latest_msg[0], overall_earliest_msg[0])
-            # print('height %s : %s' %(height, millisecond2time_format(millisecond_interval)))
-            broadcasting_time.append(millisecond_interval)
-        broadcasting_time.sort()
-        average, median = get_average_median(broadcasting_time)
-        print('最短时间: %s' %millisecond2time_format(broadcasting_time[0]))
-        print('最长时间: %s' %millisecond2time_format(broadcasting_time[-1]))
+            block_queue.put(height)
+        processes = []
+        processor_num = cpu_count()
+        for i in range(processor_num):
+            p = Process(target=calc_broadcasting_time, args=(block_queue, broadcasting_time_queue, all_block_dict_list))
+            p.start()
+            processes.append(p)
+        for process in processes:
+            process.join()
+        broadcasting_time_list = []
+        while True:
+            broadcasting_time_list.append(broadcasting_time_queue.get())
+            if broadcasting_time_queue.empty():
+                break
+        broadcasting_time_list.sort()
+        average, median = get_average_median(broadcasting_time_list)
+        print('最短时间: %s' %millisecond2time_format(broadcasting_time_list[0]))
+        print('最长时间: %s' %millisecond2time_format(broadcasting_time_list[-1]))
         print('平均值:   %s' %millisecond2time_format(average))
         print('中位数:   %s' %millisecond2time_format(median))
-
+        
     print('分析用时:', time.time()-start_time)
 
 if __name__ == '__main__':
